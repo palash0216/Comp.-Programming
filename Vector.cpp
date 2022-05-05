c9d6c15d24a19e59ec8568c93772a652e7b52bb9
Palash0216 <palashmishra54@gmai.com>
2022-05-05 17:40:28 +0530

    Second Commit

--- Vector.cpp
+++ Vector.cpp
@@ -50,22 +50,5 @@ int main()
     cout << v.capacity() << endl;
     cout << v.max_size() << endl;
 
-    vector<int> dog{1, 2, 3, 10, 14};
-    dog.push_back(16); // O(1) time
-    for (auto x : dog)
-        cout << x << " ";
-
-    dog.pop_back();
-    dog.pop_back();
-    dog.pop_back();
-    for (auto x : dog)
-        cout << x << " ";
-
-    // d.pop_back();//to remove element;
-    // d.resize(18);//capacity will change if the size will increasing
-    // d.clear();//Remove all the elements, this doesn't delete the memory occupied by the array
-    // d.empty(); // make the vector empty
-    // d.frongt();// gives the front element
-    // d.back(); // gives the element at the back;
-    // d.resume(1000); // to avoid doubling the size of the vector
+    
 }
